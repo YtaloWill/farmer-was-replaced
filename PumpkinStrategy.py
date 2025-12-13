ac445cb9ad4942fe33435c3d67adffd7d5913c08
YtaloWill <ytalowillgloria@gmail.com>
2025-12-13 17:37:21 -0300

    add punpkim strategy multi drone

--- PumpkinStrategy.py
+++ PumpkinStrategy.py
@@ -5,16 +5,16 @@ def setup(size):
 		Core.move_x_by_y(Core.check_and_till, size, size)
 		for i in range(size):
 			move(West)
-
-def run(size):
+def run(size, use_fertilizer):
 	while True:
 		setup(size)
 		for i in range(size):
 			for j in range(size):
 				while not can_harvest():
 					plant(Entities.Pumpkin)
-					#use_item(Items.Fertilizer)
-					if get_water() < 0.1:
+					if use_fertilizer:	
+						use_item(Items.Fertilizer)
+					elif get_water() < 0.1:
 						use_item(Items.Water)
 				move(North)
 			for i in range(size):
@@ -23,3 +23,41 @@ def run(size):
 		for i in range(size):
 			move(West)
 		harvest()
+
+
+def _setup():
+	Core.check_and_till()
+
+def _plant_pumpkim():
+	while not can_harvest():
+		plant(Entities.Pumpkin)
+		if get_water() < 0.5:
+			use_item(Items.Water)
+#		use_item(Items.Fertilizer)
+		
+
+def _execute(lx, ly):
+	def a():
+		if get_ground_type() != Grounds.Soil:
+			Core.move_x_by_y(_setup, lx, ly)
+		Core.move_to(get_pos_x()-lx, get_pos_y())
+		Core.move_x_by_y(_plant_pumpkim, lx, ly)
+	return a
+
+def run_multithread():
+	splited = get_world_size()/max_drones()
+	for i in range(max_drones()-1):
+		spawn_drone(_execute(splited, get_world_size()))
+		for _ in range(splited):
+			move(East)
+	_execute(splited, get_world_size())()
+	Core.move_to(0,0)
+	first = measure()
+	while first == None:
+		first = measure()
+	move(West)
+	move(South)
+	while True:
+		if measure() == first:
+			break
+	harvest()
