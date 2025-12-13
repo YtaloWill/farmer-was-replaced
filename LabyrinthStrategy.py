c629bf71927136d6668764d3881660f5f58e7d84
YtaloWill <ytalowillgloria@gmail.com>
2025-12-13 17:14:01 -0300

    optimize labyrinth to use more drones

--- LabyrinthStrategy.py
+++ LabyrinthStrategy.py
@@ -1,3 +1,5 @@
+# TODO: reuse labyrinth
+
 def _build():
 	plant(Entities.Bush)
 	substance = get_world_size() * 2**(num_unlocked(Unlocks.Mazes) - 1)
@@ -24,31 +26,35 @@ def _get_position_direction(direction):
 	y = _clamp(0, get_world_size(), current[1])
 	return (x, y)
 
+def _execute(moves):
+	def _search():
+		while True:
+			if get_entity_type() == Entities.Treasure:
+				harvest()
+				break
+	
+			maze[(get_pos_x(), get_pos_y())] = True
+			moved = False
+			if get_entity_type() == Entities.Hedge:
+				for m in moves:
+					pos = (get_pos_x(), get_pos_y())
+					already_pass = _get_position_direction(m) in maze and maze[_get_position_direction(m)]
+					if can_move(m) and not already_pass:
+						move(m)
+						backtrack.append(m)
+						moved = True
+						break
+				if not moved:
+					step = backtrack.pop()
+					move(backstep[step])
+
+	backstep = {North:South, South:North, West:East, East:West}
+	backtrack = []
+	maze = {}
+	return _search
+
 def run():
 	if get_entity_type() != Entities.Treasure and get_entity_type() != Entities.Hedge:
 		_build()
-	
-	backstep = {North:South, South:North, West:East, East:West}
-	backtrack = []
-	moves = [West, North, South, East]
-	maze = {}
-
-	while True:
-		if get_entity_type() == Entities.Treasure:
-			harvest()
-			break
-
-		maze[(get_pos_x(), get_pos_y())] = True
-		moved = False
-		if get_entity_type() == Entities.Hedge:
-			for m in moves:
-				pos = (get_pos_x(), get_pos_y())
-				already_pass = _get_position_direction(m) in maze and maze[_get_position_direction(m)]
-				if can_move(m) and not already_pass:
-					move(m)
-					backtrack.append(m)
-					moved = True
-					break
-			if not moved:
-				step = backtrack.pop()
-				move(backstep[step])
+	spawn_drone(_execute([East, South, North, West]))
+	_execute([West, North, South, East])()
